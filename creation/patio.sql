3352e14bc1460c28d2fcf12704afb5267ab02647
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2024-06-11 20:13:40 -0300

    Update patio.sql

--- creation/patio.sql
+++ creation/patio.sql
@@ -1,3 +1,12 @@
+-- INTEGRANTES:
+-- Alex Teixeira, DRE: 117036607
+-- Felipe Augusto de Miranda Villela, DRE: 114080437
+-- Felipe Pêpe da Silva Oliveira, DRE: 123589472
+-- João Pedro Martins Filipe Figueiredo Matos Menezes Pereira, DRE: 123642929
+-- Karen Silva Pacheco, DRE: 123476904
+-- Lucas Tavares, DRE: 120152739
+
+
 CREATE TABLE Patio (
   PatioID INT PRIMARY KEY,
   Nome VARCHAR(255) NOT NULL,
